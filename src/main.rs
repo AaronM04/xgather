ad4283a7c0f6b25bb414c9b496a55516c8cb29e4
Aaron Miller <aaron@iforgotmy.name>
2020-08-27 19:23:09 -0700

    everything but the real pointer position (need XQueryPointer)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use thiserror::Error;
 use std::ffi::{c_void, CString};
 use std::fmt;
 use std::mem;
-use std::os::raw::c_uint;
+use std::os::raw::{c_int, c_uint};
 use std::ptr;
 use std::rc::Rc;
 use std::slice;
@@ -19,17 +19,20 @@ pub enum XError {
     QueryRootTreeFail,
     #[error("failed to get window info / attributes")]
     WinInfoFail,
+    #[error("failed to move window {}", window_id)]
+    MoveWindowFail { window_id: u64 },
 }
 
 struct Display {
     display: *mut X::_XDisplay,
-    xlib: Rc<X::Xlib>,
+    pub xlib: Rc<X::Xlib>,
+    pub screen: c_int,
 }
 
 impl Display {
     fn root_tree(&self) -> Result<Tree, XError> {
         let root_win = unsafe { (self.xlib.XDefaultRootWindow)(self.display) };
-        // we won't use these two
+        // we won't use root_return or parent_return
         let mut root_return = mem::MaybeUninit::<X::Window>::uninit();
         let mut parent_return = mem::MaybeUninit::<X::Window>::uninit();
         let mut children = mem::MaybeUninit::<*mut X::Window>::uninit();
@@ -73,6 +76,43 @@ impl Display {
             viewable: attr.map_state == X::IsViewable,
             wm_should_ignore: attr.override_redirect != 0,
         })
+    }
+
+    fn move_window(
+        &self,
+        wininfo: &WinInfo,
+        xoffset: c_int,
+        yoffset: c_int,
+    ) -> Result<WinInfo, XError> {
+        let value_mask = 0x3; // update X and Y; see https://tronche.com/gui/x/xlib/window/configure.html#XWindowChanges
+                              // The following is not actually mutated
+        let mut win_changes = X::XWindowChanges {
+            x: wininfo.x + xoffset,
+            y: wininfo.y + yoffset,
+            width: 0,
+            height: 0,
+            border_width: 0,
+            sibling: 0,
+            stack_mode: 0,
+        };
+        let status = unsafe {
+            (self.xlib.XReconfigureWMWindow)(
+                self.display,
+                wininfo.id,
+                self.screen,
+                value_mask,
+                &mut win_changes as *mut X::XWindowChanges,
+            )
+        };
+        if status == 0 {
+            return Err(XError::MoveWindowFail {
+                window_id: wininfo.id,
+            });
+        }
+        let mut new_wininfo = wininfo.clone();
+        new_wininfo.x += xoffset;
+        new_wininfo.y += yoffset;
+        Ok(new_wininfo)
     }
 }
 
@@ -149,12 +189,40 @@ fn connect(xlib: Rc<X::Xlib>, display_name: Option<&str>) -> Result<Display, XEr
         return Err(XError::ConnectFail);
     }
 
-    Ok(Display { display, xlib })
+    // Store the screen because it'll come in handy
+    let screen = unsafe { (xlib.XDefaultScreen)(display) };
+
+    Ok(Display {
+        display,
+        xlib,
+        screen,
+    })
+}
+
+fn calc_1d_offset(high: c_int, low: c_int, target: c_int) -> Option<c_int> {
+    if target < low {
+        Some(target - low)
+    } else if target > high {
+        Some(target - high)
+    } else {
+        None
+    }
+}
+
+fn calc_2d_offset(wininfo: &WinInfo, x: c_int, y: c_int) -> Option<(c_int, c_int)> {
+    let xoffset = calc_1d_offset(wininfo.x + wininfo.width - 1, wininfo.x, x);
+    let yoffset = calc_1d_offset(wininfo.y + wininfo.height - 1, wininfo.y, y);
+    if xoffset.is_some() && yoffset.is_some() {
+        Some((xoffset.unwrap(), yoffset.unwrap()))
+    } else {
+        None
+    }
 }
 
 fn main() {
     let xlib = X::Xlib::open().unwrap();
-    let display = connect(Rc::new(xlib), None).unwrap();
+    let xlib = Rc::new(xlib);
+    let display = connect(xlib.clone(), None).unwrap();
 
     let tree = display.root_tree().unwrap();
     let slice = tree.as_slice();
@@ -164,13 +232,30 @@ fn main() {
         .map(|win| display.wininfo(*win).unwrap())
         .collect();
     let mut filtered_count = 0;
-    for (i, wininfo) in wininfos
-        .iter()
-        .filter(|&wininfo| !wininfo.wm_should_ignore && wininfo.width > 10 && wininfo.height > 10)
-        .enumerate()
-    {
+    let filtered_wininfos: Vec<_> = wininfos
+        .into_iter()
+        .filter_map(|wininfo| {
+            // using filter_map instead of filter because of weird borrow issue I don't have time
+            // to debug
+            if !wininfo.wm_should_ignore && wininfo.width > 10 && wininfo.height > 10 {
+                Some(wininfo)
+            } else {
+                None
+            }
+        })
+        .collect();
+    for (i, wininfo) in filtered_wininfos.iter().enumerate() {
         println!("{:>4}: {}", i, wininfo);
         filtered_count += 1;
     }
     println!("FILTERED COUNT: {}", filtered_count);
+
+    println!("asking the window manager to move windows...");
+    let (pointerx, pointery) = (500, 2000); //XXX get pointer pos
+    for wininfo in filtered_wininfos.iter() {
+        if let Some((xoffset, yoffset)) = calc_2d_offset(wininfo, pointerx, pointery) {
+            println!("MOVE win with id 0x{:0>7x} by ({:>5}, {:>5})", wininfo.id, xoffset, yoffset);
+            display.move_window(wininfo, xoffset, yoffset).unwrap();
+        }
+    }
 }
